6f3fb69e688c3954ad415d6fec313cd4e819b168
Nicole Burg <emailregisterlogin@gmail.com>
2022-01-12 17:07:40 -0500

    Move contents in FlightComputer wrapper

--- flight_computer/main.cpp
+++ flight_computer/main.cpp
@@ -175,58 +175,76 @@ struct Controller {
     }
 };
 
-Sensors sensors;
-Vector3<double> integral;
-Filter<double> y_filter;
-Filter<double> z_filter;
-// Initial orientation quaternion of the hopper.
-Quaternion<double> base{ 0.71, 0.71, 0.0, 0.0 };
-// 0.122173 radians is 7 in degrees.
-constexpr double maximum_angle = 0.122173; // unit: radians.
+struct FlightComputer {
+    Sensors sensors;
+    Vector3<double> integral;
+    Filter<double> y_filter;
+    Filter<double> z_filter;
+    // Initial orientation quaternion of the hopper.
+    Quaternion<double> base{ 0.71, 0.71, 0.0, 0.0 };
+    // 0.122173 radians is 7 in degrees.
+    double maximum_angle = 0.122173; // unit: radians.
+    
+    void Init() {
+        sensors.Init();
+    }
+    
+    void Update() {
+        // Microcontroller time step calculation.
+        // TODO: Change this to compute time from last update.
+        auto dt = 0.0001;
+    
+        // Raw sensor data retrieval.
+        auto raw_ang_pos = sensors.GetAngularPosition().DegreesToRadians();
+        auto raw_ang_vel = sensors.GetAngularVelocity().DegreesToRadians();
+        
+        // Filtering of raw sensor data.
+        y_filter.UpdateValue(raw_ang_vel.y);
+        z_filter.UpdateValue(raw_ang_vel.z);
+        Vector3<double> processed_ang_vel{ 
+            raw_ang_vel.x, 
+            y_filter.GetValue(), 
+            z_filter.GetValue() 
+        };
+        
+        // Orientation resolution to prevent gimbal lock.
+        auto q_dot = base.Differentiated(processed_ang_vel);
+        base += q_dot * dt;
+        // TODO: Research, this normalization may not be required.
+        base = base.Normalized();
+        auto ang_pos = Vector3<double>(base).RadiansToDegrees();
+    
+        // Integrated control gains.
+        integral.y += Controller::INTEGRAL_GAIN * ang_pos.y * dt;
+        integral.z += Controller::INTEGRAL_GAIN * ang_pos.z * dt;
+        
+        // LQR.
+        auto control_y = Controller::LQR(ang_pos.y, processed_ang_vel.y);
+        auto control_z = Controller::LQR(ang_pos.z, processed_ang_vel.z);
+        control_y += integral.y;
+        control_z += integral.z;
+        // Ensure control angle is never above or below the maximum controllability.
+        control_y = Clamp(control_y, -maximum_angle, maximum_angle); 
+        control_z = Clamp(control_z, -maximum_angle, maximum_angle);
+    
+        // Update servos with latest values.
+        // TODO: Make this write the calculated control angles:
+        // Servos.write(U_y, U_z)
+    }
+};
+
+FlightComputer fc;
 
 void setup() {
-    sensors.Init();
+    fc.Init();
 }
 
 void loop() {
-    // Microcontroller time step calculation.
-    // TODO: Change this to compute time from last update.
-    auto dt = 0.0001;
+    fc.Update();
+}
 
-    // Raw sensor data retrieval.
-    auto raw_ang_pos = sensors.GetAngularPosition().DegreesToRadians();
-    auto raw_ang_vel = sensors.GetAngularVelocity().DegreesToRadians();
-    
-    // Filtering of raw sensor data.
-    y_filter.UpdateValue(raw_ang_vel.y);
-    z_filter.UpdateValue(raw_ang_vel.z);
-    Vector3<double> processed_ang_vel{ 
-        raw_ang_vel.x, 
-        y_filter.GetValue(), 
-        z_filter.GetValue() 
-    };
-    
-    // Orientation resolution to prevent gimbal lock.
-    auto q_dot = base.Differentiated(processed_ang_vel);
-    base += q_dot * dt;
-    // TODO: Research, this normalization may not be required.
-    base = base.Normalized();
-    auto ang_pos = Vector3<double>(base).RadiansToDegrees();
-
-    // Integrated control gains.
-    integral.y += Controller::INTEGRAL_GAIN * ang_pos.y * dt;
-    integral.z += Controller::INTEGRAL_GAIN * ang_pos.z * dt;
-    
-    // LQR.
-    auto control_y = Controller::LQR(ang_pos.y, processed_ang_vel.y);
-    auto control_z = Controller::LQR(ang_pos.z, processed_ang_vel.z);
-    control_y += integral.y;
-    control_z += integral.z;
-    // Ensure control angle is never above or below the maximum controllability.
-    control_y = Clamp(control_y, -maximum_angle, maximum_angle); 
-    control_z = Clamp(control_z, -maximum_angle, maximum_angle);
-    
-    // Update servos with latest values.
-    // TODO: Make this write the calculated control angles:
-    // Servos.write(U_y, U_z)
+int main() {
+    setup();
+    loop();
+    return 0;
 }
