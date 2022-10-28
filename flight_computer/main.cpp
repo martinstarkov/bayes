3a43b272af4ed737af50ea4dae9e393c80add60c
Garcia242 <70655945+Garcia242@users.noreply.github.com>
2022-10-28 22:19:19 +0100

    BMP code added
    BMO code added, not sure if it works

--- flight_computer/main.cpp
+++ flight_computer/main.cpp
@@ -236,33 +236,7 @@ struct BMP {
         Adafruit_BMP280 bmp;
         
         bmp.begin();
-        
-            Serial.begin(9600);
-            while ( !Serial ) delay(100);   // wait for native usb
-            Serial.println(F("BMP280 test"));
-            unsigned status;
-            //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
-            status = bmp.begin();
-            if (!status) {
-            Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                             "try a different address!"));
-            Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
-            Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-            Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-            Serial.print("        ID of 0x60 represents a BME 280.\n");
-            Serial.print("        ID of 0x61 represents a BME 680.\n");
-            while (1) delay(10);
-            }
-
-         /* Default settings from datasheet. */
-            bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-        
-        /* Adjusted to local forecast! */
-            Serial.println(" m");
+     
         // TODO: Implement BMP instance here.
         // TODO: Check any calibration needed?
         // bmp.setSettings(A, B, C)???? something like this?
@@ -274,7 +248,6 @@ struct BMP {
         return bmp.getTemperature();
     }
     int GetAltitude() {
-        
         
         Serial.print(bmp.readAltitude());
         // TODO: Get this working x
