b698d723b44870429cca751a20321e35f9f86ad4
Garcia242 <70655945+Garcia242@users.noreply.github.com>
2022-11-03 20:14:06 +0000

    Servo with input of the BNO code added

--- flight_computer/servo_test.cpp
+++ flight_computer/servo_test.cpp
@@ -23,3 +23,40 @@ void loop(){
    Servo1.write(180); 
    delay(1000); 
 }
+
+
+
+// sertvio with input from BNO
+
+
+#include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+sensors_event_t event;
+
+Servo Servo1;
+Servo Servo2;
+/* Initialise the sensor */
+
+
+  void setup(void) {
+    Serial.begin(115200);
+    
+    if (!bno.begin()) {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    }
+    
+    Servo1.attach(10);
+    Servo2.attach(11);
+  }
+
+  void loop() {
+    bno.getEvent(&event);
+    Servo1.write(event.orientation.y);
+    Servo2.write(event.orientation.z);
+  }
