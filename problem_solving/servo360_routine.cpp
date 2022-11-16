c03c8558bdd96cd3ce2ea86b793749088fb0a6b1
omnomnomnomnomno <94877472+omnom06@users.noreply.github.com>
2022-11-16 21:40:40 +0000

    Added Servo360 routine

---
+++ problem_solving/servo360_routine.cpp
@@ -0,0 +1,34 @@
+#include <math.h>
+#include <Servo.h>
+
+void servo360(float *servo1_Angle, float *servo2_Angle);
+
+class Routine() {
+public:
+  Servo servo1, servo2;
+  int servo_Pin1 = 10, servo_Pin2 = 11;
+  float servo1_Angle, servo2_Angle;
+
+  void servo360(float servo1_Angle, float servo2_Angle) {
+    static int counter;
+
+    counter = (counter <= 360) ? counter + 1 : 0;
+    (*servo1_Angle) = sin(counter);
+    (*servo2_Angle) = cos(counter);
+  }
+}
+
+Routine servo_Module;
+
+void setup() {
+    Serial.begin(9600);
+    servo1.attach(servo_Module.servo_Pin1);
+    servo2.attach(servo_Module.servo_Pin2);
+}
+
+void loop() {
+    servo_Module.servo360(servo_Module.servo1_Angle, servo_Module.servo2_angle);
+    servo1.write(servo_Module.servo1_Angle);
+    servo2.write(servo_Module.servo2_angle);
+    delay(100);
+}
